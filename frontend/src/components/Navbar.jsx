1801a01f61a916a7ab226de2c0e62a89c0a1214b
mimi3c <151084513+mimi3c@users.noreply.github.com>
2024-03-09 00:48:44 -0500

    more setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <div>
+            <nav className="bg=white">
+                test
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
