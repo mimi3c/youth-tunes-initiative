f307bbb3ab3b751d04bd29012e711b63a87dbae6
amagdum12 <98058388+amagdum12@users.noreply.github.com>
2024-04-20 16:21:24 -0400

    added vid, links to navbar and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../images/YTI_logo.svg";
 
 const Footer = () => {
     return(
@@ -8,14 +9,17 @@ const Footer = () => {
                     Contact: 
                 </div>
                 <div className="">
-                    youthtunesinitiative@email.com <br />
-                    123-456-7890
+                    youth.tunes.initiative@email.com <br />
+                    908-873-2857
                 </div>
             </div>
             <div className="flex-1">
                 Address line 1 <br />
                 City State 12345 <br />
                 idrk what to put here
+            </div>
+            <div className="">
+                <img src={logo} alt="logo" className="h-20 my-1"/>
             </div>
         </div>
     )
