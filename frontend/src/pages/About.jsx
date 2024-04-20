e6c9fdafb5a15df3acc241ddf825d3a9a5238736
amagdum12 <98058388+amagdum12@users.noreply.github.com>
2024-04-20 08:45:50 -0400

    set up about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,14 +1,28 @@
-// About.js
-
 import React from 'react';
+import logo from "../images/YTI_logo.svg";
 
 const About = () => {
-    return (
-        <div>
-            <h1>About Us</h1>
-            <p>This is the about page content.</p>
+  return (
+    <div>
+      {/* Other content in the About page */}
+      <div className="bg-night py-40 grid justify-center items-center">
+        <h1 className="text-5xl font-caprasimo uppercase text-center text-sea font-shrikhand">Our Volunteers</h1>
         </div>
-    );
+
+        <div className="flex justify-center items-center h-80"> {/* Increased height as needed */}
+            <img src={logo} alt="logo" className="h-48 object-cover mx-12"/> {/* Increased height and margin */}
+            <img src={logo} alt="logo" className="h-48 object-cover mx-12"/> {/* Increased height and margin */}
+            <img src={logo} alt="logo" className="h-48 object-cover mx-12"/> {/* Increased height and margin */}
+        </div>
+
+
+
+
+    </div>
+
+
+      
+  );
 }
 
 export default About;
